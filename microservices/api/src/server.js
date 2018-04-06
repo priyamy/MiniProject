1fa24e498fd7f76e7efe61350b42a5c5273b91a4
priyamy <priyamvy@gmail.com>
2018-04-06 16:26:59 +0530

    Modified server.js

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get('/',(req,res) => res.send('Hello World'));
+app.get('/',(req,res) => res.send('Welcome. This is our Mini Project'));
 
 app.post('/dbupdate', (req,res) => {
 
